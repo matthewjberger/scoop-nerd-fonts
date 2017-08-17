b38b24d15dba34b33c9f5d4da8238a6f27688b06
Matthew J. Berger <matthewberger@nevada.unr.edu>
2017-08-17 14:49:30 -0700

    Update installation script to copy all fonts at once thanks to r15ch13 on lukesampson/scoop-extras#500.

--- scripts/install-nerd-fonts.ps1
+++ scripts/install-nerd-fonts.ps1
@@ -2,19 +2,6 @@ param([String]$dir)
 
 $Shell = New-Object -ComObject Shell.Application
 $SystemFontsFolder = $Shell.Namespace(0x14)
-$SystemFontsPath = $Shell.Namespace(0x14)
-$Fonts = Get-ChildItem $dir -Filter "*Windows Compatible*"
-
-foreach($Font in $Fonts)
-{
-    $targetPath = Join-Path $SystemFontsPath $Font.Name
-    if(Test-Path $targetPath)
-    {
-        Remove-Item $targetPath -force
-        Copy-Item $Font.FullName $targetPath -Force
-    }
-    else
-    {
-        $SystemFontsFolder.CopyHere($Font.FullName)
-    }
-}
+$Fonts = $Shell.Namespace($dir).Items()
+$Fonts.Filter(64, '*Windows Compatible.*')
+$SystemFontsFolder.CopyHere($Fonts)
