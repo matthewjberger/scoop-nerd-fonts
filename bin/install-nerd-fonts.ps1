3a7d157e376db5353d5c7c6e6931f3796e077ee3
Matthew J. Berger <matthewberger@nevada.unr.edu>
2017-08-18 23:08:51 -0700

    Update scripts.

--- bin/install-nerd-fonts.ps1
+++ bin/install-nerd-fonts.ps1
@@ -3,5 +3,5 @@ param([String]$dir)
 
 Get-ChildItem $dir -filter '*Windows Compatible.*' | ForEach-Object {
     New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts' -Name $_.Name.Replace($_.Extension, ' (TrueType)') -Value $_.Name -Force | Out-Null
-    Copy-Item "$dir/$_" -destination "$env:windir\Fonts"
+    Copy-Item "$dir\$_" -destination "$env:windir\Fonts"
 }
